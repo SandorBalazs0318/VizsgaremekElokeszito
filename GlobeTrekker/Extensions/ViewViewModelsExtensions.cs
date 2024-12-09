372f974599c821d15ea339f63e66b5799c41c639
Sándor Balázs <c9sanbal@vasvari.org>
2024-12-09 20:15:55 +0100

    halad

---
+++ GlobeTrekker/Extensions/ViewViewModelsExtensions.cs
@@ -0,0 +1,39 @@
+﻿using GlobeTrekker.ViewModels;
+using GlobeTrekker.ViewModels.ControlPanel;
+using GlobeTrekker.ViewModels.Login;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobeTrekker.Extensions
+{
+    public static class ViewViewModelsExtensions
+    {
+        public static void ConfigureViewViewModels(this IServiceCollection services)
+        {
+            // MainView
+            services.AddSingleton<MainViewModel>();
+            services.AddSingleton<MainView>(s => new MainView()
+            {
+                DataContext = s.GetRequiredService<MainViewModel>()
+            });
+
+            // LoginView
+            services.AddSingleton<LoginViewModel>();
+            services.AddSingleton<LoginView>(s => new LoginView()
+            {
+                DataContext = s.GetRequiredService<LoginViewModel>()
+            });
+
+            // ControlPanel
+            services.AddSingleton<ControlPanelViewModel>();
+            services.AddSingleton<ControlPanelView>(s => new ControlPanelView()
+            {
+                DataContext = s.GetRequiredService<ControlPanelViewModel>()
+            });
+        }
+    }
+}
---
+++ GlobeTrekker/ViewModels/Base/BaseViewModel.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace GlobeTrekker.ViewModels.Base
+{
+    public abstract class BaseViewModel : ObservableObject
+    {
+    }
+}
